2ec1b714e3259ddc885bf4f6ece3b61ad24a2b95
Ga2nibal <sergei.fediukovich@gmail.com>
2015-11-29 01:36:31 +0200

    ..

--- src/Main.hs
+++ src/Main.hs
@@ -1,6 +1,81 @@
-import System.Environment
+import Control.Applicative
+import Options
+import Modules.FCM
+import Modules.CsvParser
+import Modules.Distance
+import System.IO.Error
+import Control.Exception
+
+data MainOptions = MainOptions
+   { inputFile :: String,
+    outputFile :: String,
+    numOfClaster :: Int,
+    accuracy :: Double,
+    metric :: Metric,
+    isRandCenters :: Bool,
+    separator :: String,
+    isIgnoreHeader :: Bool,
+    isIgnoreFirstColumn :: Bool
+   ,isIgnoreLastColumn :: Bool }
+
+instance Options MainOptions where
+    defineOptions = pure MainOptions
+     	<*> simpleOption "inputFile" ""
+            "Input filepath"
+        <*> simpleOption "outputFile" ""
+            "Output filepath"
+        <*> simpleOption "numOfClaster" 0
+            "number of clusters"
+        <*> simpleOption "accuracy" 0.0001
+            "accuracy"
+        <*> defineOption (optionType_enum "metric") (\o -> o 
+        	{ optionLongFlags = ["metric"] 
+        	 , optionDefault = Euclid 
+        	})
+        <*> simpleOption "isRandCenters" False
+            "isRandCenters"
+        <*> simpleOption "separator"  ","
+            "column separator"
+        <*> simpleOption "isIgnoreHeader" False
+            "Ignore header row"
+
+        <*> simpleOption "isIgnoreFirst" False
+            "Ignore first column"
+        <*> simpleOption "isIgnoreLast" False
+            "Ignore last column"
 
 main::IO()
-main = getArgs >>= print . haqify . head
+main = runCommand $ \opts args -> do
+    redirectResultToOutput opts `catch` catcherror
 
-haqify s = "Haqfad! " ++ s
+catcherror :: IOError -> IO()
+catcherror ex = ioError ex
+
+redirectResultToOutput :: MainOptions -> IO()
+redirectResultToOutput param 
+    | "" == (outputFile param) = do
+        cl <- solve param
+        putStrLn (show $ matrixWithLineSeparator cl)
+    | otherwise = do
+        cl <- solve param
+        writeFile (outputFile param) (show $ matrixWithLineSeparator cl)
+
+matrixWithLineSeparator :: [[Double]] -> String
+matrixWithLineSeparator [] =  ""
+matrixWithLineSeparator m = show (head m) ++ "\\r\\n" ++ matrixWithLineSeparator (tail m)
+
+solve :: MainOptions -> IO [[Double]]
+solve param = do
+        let fcm = FcmParams { clustersCount = (numOfClaster param), dvtn = (accuracy param), distance_func = (metric param), init_method = (isRandCenters param) }
+        parsed <- parse_from_csv param
+        cluserized <- solveFcm parsed fcm
+        return cluserized
+
+
+parse_from_csv :: MainOptions -> IO [[Double]]
+parse_from_csv param = do
+    let parseOpts = ParserOptions { Modules.CsvParser.separator = ((head (Main.separator param)) :: Char), f_line = (isIgnoreHeader param), f_column = (isIgnoreFirstColumn param), l_column = (isIgnoreLastColumn param) }
+    source <- readFile (inputFile param)
+    csvResult <- parseCsv source parseOpts
+    case csvResult of Left errorMessage -> error errorMessage
+                      Right objects -> return objects
--- src/Modules/CsvParser.hs
+++ src/Modules/CsvParser.hs
@@ -5,44 +5,40 @@ module Modules.CsvParser (
 
 
 
-import Data.Char (ord)
-import Data.ByteString.Char8 (pack)
-import Data.ByteString.Internal (unpackBytes)
-import qualified Data.ByteString.Lazy as BL
 import Data.Csv
-import qualified Data.Vector as V
-import Data.Matrix as M
+import Data.Char
+import Data.Vector as V
+import Data.ByteString.Internal as BSI
+import Data.ByteString.Lazy as BSL
+import Data.ByteString.Char8 as BSC
 
+data ParserOptions = ParserOptions { separator :: Char, f_line,f_column,l_column :: Bool}
+                                               
+parseCsv :: String -> ParserOptions -> IO(Either String [[Double]])
+parseCsv from_file csvPrms = do
+    let options = DecodeOptions { decDelimiter = fromIntegral $ ord $ separator csvPrms }
+        csv_data = BSL.pack $ BSI.unpackBytes $ BSC.pack from_file
+        parsed = if (f_line csvPrms) 
+          then decodeWith options NoHeader csv_data :: Either String (Vector (Vector String)) 
+          else decodeWith options HasHeader  csv_data :: Either String (Vector (Vector String))
+    case parsed of Left error -> return $ Left error
+                   Right parsed_data -> do
+                        let checked = convertToDoubleList(removeColumns (l_column csvPrms) (f_column csvPrms) parsed_data)
+                        return $ Right $ checked
 
+removeColumns :: Bool -> Bool -> Vector (Vector a) -> Vector (Vector a)
+removeColumns True True m = removeHeads (removeLasts m)
+removeColumns True False m = removeHeads m
+removeColumns False True m = removeLasts m
+removeColumns False False m = m
 
-data ParserOptions = ParserOptions {  delimiter :: Char,
-                                      isIgnoreHeader :: Bool,
-                                      isIgnoreFirstColumn :: Bool,
-                                      isIgnoreLastColumn :: Bool  }
+removeHeads :: Vector (Vector a) -> Vector (Vector a)
+removeHeads arr = V.map V.init arr
 
+removeLasts :: Vector (Vector a)-> Vector (Vector a)
+removeLasts arr = V.map V.tail arr
 
-
-parseCsv :: String -> ParserOptions -> IO(Either String (Matrix Double))
-parseCsv csvData parserOpts = do
-  let decodeOptions = DecodeOptions { decDelimiter = fromIntegral (ord $ delimiter parserOpts) }
-      csvBytes = BL.pack . unpackBytes . pack $ csvData
-
-  let result = if isIgnoreHeader parserOpts
-                  then decodeWith decodeOptions HasHeader csvBytes
-                  else decodeWith decodeOptions NoHeader csvBytes
-
-  case result of
-      Left exception -> return (Left exception)
-      Right table -> let withoutFirstColumn = if isIgnoreFirstColumn parserOpts then
-                                                V.map V.tail table else table
-
-                         withoutLastColumn = if isIgnoreLastColumn parserOpts then
-                                                V.map V.init withoutFirstColumn else withoutFirstColumn
-                     in return (Right $ toDoubleMatrix withoutLastColumn)
-
-
-
-toDoubleMatrix :: V.Vector(V.Vector String) -> Matrix Double
-toDoubleMatrix table =  fromLists doubleList
-                        where doublesV = V.map (V.map $ \ x -> read x :: Double) table
-                              doubleList = V.toList $ V.map V.toList doublesV
+convertToDoubleList :: Vector (Vector String) -> [[Double]]
+convertToDoubleList v 
+    | V.null v = []
+    | otherwise = Prelude.map read (toList (V.head v)) : convertToDoubleList (V.tail v)
